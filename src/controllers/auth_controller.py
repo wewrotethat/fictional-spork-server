c3afbff3ab626605a7b4b460af98e013b6082b12
Yabsra <yabsra23@gmail.com>
2022-05-29 13:37:02 +0300

    fix: change json encoder

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -5,7 +5,7 @@ import os
 import jwt
 import bcrypt
 from flask import Response, request
-from flask_restful import Resource
+from flask_restful import Resource, output_json
 
 from src.models.user import User
 
@@ -32,9 +32,10 @@ class AuthController(Resource):
                     os.environ["JWT_SECRET"],
                     algorithm="HS256",
                 )
-                return Response(
-                    JSONEncoder().encode({"success": True, "token": jwt_token}),
-                    status=200,
+                return output_json(
+                    code=200,
+                    data={"success": True, "token": jwt_token},
+                    headers={"content-type": "application/json"},
                 )
             else:
                 return error_response
--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,8 +1,9 @@
 import bcrypt
 from json import JSONEncoder
+from black import out
 from flask import request, Response
 from src.models.user import User
-from flask_restful import Resource
+from flask_restful import Resource, output_json
 from mongoengine.errors import ValidationError, NotUniqueError
 
 
@@ -14,19 +15,28 @@ class UsersController(Resource):
             byte_password: bytearray = user.password.encode("utf-8")
             salt: str = bcrypt.gensalt()
             hashed_password: str = bcrypt.hashpw(byte_password, salt)
-            user.password = hashed_password.decode('utf8')
+            user.password = hashed_password.decode("utf8")
             user.save()
             user_dict: dict = user.__dict__()
-            user_json: str = JSONEncoder().encode(user_dict)
-            return Response(user_json, mimetype="application/json", status=201)
+            return output_json(
+                data=user_dict, code=201, headers={"content-type": "application/json"}
+            )
         except ValidationError as e:
-            return Response(JSONEncoder().encode({"error": str(e)}), status=400)
+            return output_json(
+                data={"error": str(e)},
+                code=400,
+                headers={"content-type": "application/json"},
+            )
         except NotUniqueError as e:
-            return Response(JSONEncoder().encode({"error": str(e)}), status=400)
+            return output_json(
+                data={"error": str(e)},
+                code=400,
+                headers={"content-type": "application/json"},
+            )
         except Exception as e:
-            return Response(
-                JSONEncoder().encode({"error": "some error occurred in our servers"}),
-                status=500,
+            return output_json(
+                data={"error": "some error occurred in our servers"},
+                code=500,
             )
 
 
@@ -35,8 +45,9 @@ class UserController(Resource):
         user: User = User.objects.get(id=id)
         user.id = str(user.id)
         user_dict: dict = user.__dict__()
-        user_json: str = JSONEncoder().encode(user_dict)
-        return Response(user_json, mimetype="application/json", status=200)
+        return output_json(
+            user_dict, code=200, headers={"content-type": "application/json"}
+        )
 
     def put(self, id: str):
         data: str = request.get_json()
@@ -44,5 +55,6 @@ class UserController(Resource):
         user.update(**data)
         updated_user: User = User.objects.get(id=id)
         updated_user_dict: dict = updated_user.__dict__()
-        updated_user_json: str = JSONEncoder().encode(updated_user_dict)
-        return Response(updated_user_json, mimetype="application/json", status=200)
+        return output_json(
+            updated_user_dict, code=200, headers={"content-type": "application/json"}
+        )
