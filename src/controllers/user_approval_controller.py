eac3703c6c0a5fac251989d7ee7a196e4d9b83a9
Yabsra <yabsra23@gmail.com>
2022-06-17 22:19:49 +0300

    feat: profile verification notification (Needs testing)

--- src/controllers/user_approval_controller.py
+++ src/controllers/user_approval_controller.py
@@ -1,5 +1,6 @@
 from flask import Request, request
 from flask_restful import Resource, output_json
+from services.cloud_messaging import sendPushForTokens
 from src.models.enums.profile_verification_status import ProfileVerificationStatus
 from src.middlewares.authentication_middleware import authenticate
 from src.middlewares.authorization_middleware import authorize
@@ -17,11 +18,15 @@ class UserApprovalController(Resource):
             data: str = req.get_json()
             user_id = data["userId"]
             verification_status = data["verificationStatus"]
-            user = User.objects.get(id=user_id)
+            user: User = User.objects.get(id=user_id)
             user.profile_verification_status = ProfileVerificationStatus[
                 verification_status.upper()
             ]
             user.save()
+            sendPushForTokens(self.getTitle(user), self.getBody(user), user.device_tokens, {
+                'event': 'ACCOUNT_APPROVAL',
+                'user_id': str(user.id)
+            })
             user_dict: dict = user.__dict__()
             return output_json(
                 data=user_dict, code=201, headers={"content-type": "application/json"}
@@ -32,3 +37,14 @@ class UserApprovalController(Resource):
                 code=500,
                 headers={"content-type": "application/json"},
             )
+
+    def getTitle(_: User):
+        return f"Profile Verification Status Update"
+
+    def getBody(user: User):
+        if user.profile_verification_status == ProfileVerificationStatus.REJECTED:
+            return f"Dear {user.first_name}, your request to verify your account has been Rejected. Please resubmit a correct Medical License ID."
+        elif user.profile_verification_status == ProfileVerificationStatus.VERIFIED:
+            return f"Dear {user.first_name}, your account has been verified. You can continue to use the app."
+
+        return "Sorry to disturb you your peace. Someone in our team will be punished for this."
