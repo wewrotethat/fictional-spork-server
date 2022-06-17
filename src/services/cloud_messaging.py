20d62a415d4330dff5f3248d3ab72ac223acb995
Yabsra <yabsra23@gmail.com>
2022-06-17 22:05:21 +0300

    feat: push service (WIP)

---
+++ src/services/cloud_messaging.py
@@ -0,0 +1,18 @@
+from firebase_admin import messaging
+
+
+def sendPushForTokens(
+    title,
+    body,
+    tokens,
+    data=None,
+):
+    message = messaging.MulticastMessage(
+        notification=messaging.Notification(
+            title=title,
+            body=body,
+            data=data,
+        ),
+        tokens=tokens,
+    )
+    messaging.send_multicast(message)
